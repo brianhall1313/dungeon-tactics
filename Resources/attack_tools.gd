e8b3b478e6635d70e3d187224044b0efb7b21db2
Brian <brianhall1313@gmail.com>
2024-03-13 12:35:59 -0700

    added advantage and disadvantage, maybe made things too easy

--- Resources/attack_tools.gd
+++ Resources/attack_tools.gd
@@ -8,8 +8,16 @@ func fight(attacker,defender):
 	var attack:int
 	var defence:int
 	var damage:int
-	attack=dice_roll()+attacker.get_attack()
-	defence=dice_roll()+defender.get_attack()
+	if attacker.faction=='player':
+		attack=advantage_roll()+attacker.get_attack()
+	else:
+		attack=disadvantage_roll()+attacker.get_attack()
+	if defender.faction=='player':
+		defence=advantage_roll()+defender.get_attack()
+	else:
+		defence=disadvantage_roll()+defender.get_attack()
+	if Global.debug:
+		print('Attack: ',attack,', Defence: ',defence)
 	if attack>=defence:
 		damage=attack+attacker.get_damage_bonus()
 		print('attacker: '+attacker.character_name+ ' hits defender: '+defender.character_name)
@@ -32,6 +40,37 @@ func dice_roll():
 		return roll
 
 
+func advantage_roll():
+	var roll:int = 0
+	var roll1 = randi_range(1,20)
+	var roll2 = randi_range(1,20)
+	if roll1>roll2:
+		roll = roll1
+	else:
+		roll = roll2
+	if roll==20:
+		print('critical hit!!!')
+		return roll+crit_damage_bonus
+	else:
+		return roll
+
+
+func disadvantage_roll():
+	var roll:int = 0
+	var roll1 = randi_range(1,20)
+	var roll2 = randi_range(1,20)
+	if roll1<roll2:
+		roll = roll1
+	else:
+		roll = roll2
+	if roll==20:
+		print('critical hit!!!')
+		return roll+crit_damage_bonus
+	else:
+		return roll
+
+
+
 func multi_target_fight(attacker,defenders:Array):
 	for defender in defenders:
 		fight(attacker,defender)
@@ -40,8 +79,14 @@ func special_action(attacker,defender,action):
 	var attack:int
 	var defence:int
 	var damage:int
-	attack=dice_roll()+SpellsAndAbilities.spells_and_abilities_directory[action]['power']
-	defence=dice_roll()+defender.get_attack()
+	if attacker.faction=='player':
+		attack=advantage_roll()+SpellsAndAbilities.spells_and_abilities_directory[action]['power']
+	else:
+		attack=disadvantage_roll()+SpellsAndAbilities.spells_and_abilities_directory[action]['power']
+	if defender.faction=='player':
+		defence=advantage_roll()+defender.get_attack()
+	else:
+		defence=disadvantage_roll()+defender.get_attack()
 	if attack>=defence:
 		damage=attack+SpellsAndAbilities.spells_and_abilities_directory[action]['bonus']
 		print('attacker: '+attacker.character_name+ ' hits defender: '+defender.character_name+" with "+action)
