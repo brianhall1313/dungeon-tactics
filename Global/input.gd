2afc87064f81f31b1842daca8d400e3350653697
Brian <brianhall1313@gmail.com>
2024-06-05 15:07:30 -0700

    hold to rapid move cursor

--- Global/input.gd
+++ Global/input.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ba70c0c69ab09e6523cafd01bc94c4fa04bd57d3eb90cccbd200dde7b51a31c
-size 757
+oid sha256:b0196936cfbba491cafd2d8ab3a4ff91b60154f7e9f6cf2badf8331b37559141
+size 922
